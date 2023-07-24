517cffbb4eebd72727bb047fc01cac1fbb75bab1
ovi-khan <manikkhan2240@gmail.com>
2023-07-24 23:56:44 +0600

    admission data crate

--- index.js
+++ index.js
@@ -29,13 +29,45 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+
+    const collegeDataCollection = client.db("collegeBookingDB").collection('collegeData')
+    const admissionCollection = client.db("collegeBookingDB").collection('admission')
+
+    app.get('/collegeData', async(req, res) => {
+        const result = await collegeDataCollection.find().toArray();
+        res.send(result)
+    })
+
+    app.get('/admission', async(req, res) => {
+      const cursor = admissionCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/admission/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await admissionCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.post('/admission', async(req, res) => {
+      const newAdmission = req.body;
+      console.log(newAdmission)
+      const result = await admissionCollection.insertOne(newAdmission)
+      res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
